aa4bf9e0393a1c7c5df9d106f97053ffcab76e85
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-14 15:59:31 +0100

    edit steps to follow

---
+++ main.js
@@ -0,0 +1,28 @@
+
+//PALINDROMA
+
+//indicare all'utente di inserire una parola nel prompt 
+
+
+//creare una funzione che compara la parola palindroma con la parola inserita dall'utente
+
+
+//PARI E DISPARI
+
+// far scegliere e poi scrivere all'utente cosa sceglie tra pari e dispari
+
+//far inserire all'utente un numero da 1 a 5
+
+
+//creare una funzione che genera un numero casuale da 1 a 5 per il pc
+
+
+// fare la somma tra numero inseriti dall'utente e numero randoma generato
+
+//funzione per verificare se la somma e pari o dispari
+
+//loggare in console il vincitore
+
+
+
+
