e3bccca0444b1f0a7d9e486d69cc0b07bd269f5b
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-14 18:03:45 +0100

    fix palindroma code

--- Pari&Dispari/main.js
+++ Pari&Dispari/main.js
@@ -12,9 +12,16 @@ let sceltaUtente = prompt("Scegli 'pari' o 'dispari' e scrivilo")
 //far inserire all'utente un numero da 1 a 5
 let numeroUtente = Number(prompt("Inserisci un numero a scelta tra 1 a 5"))
 
+//verificare se il numero e pari o dispari
+
+function numeroEven()
+
 //creare una funzione che genera un numero casuale da 1 a 5 per il pc
 let numeroComputer = numeroRandom();
 console.log(`Il numero del PC è: ${numeroComputer}`)
+
+
+
 
 // fare la somma tra numero inseriti dall'utente e numero randoma generato
 
--- main.js
+++ main.js
@@ -2,24 +2,21 @@
 //PALINDROMA
 
 //indicare all'utente di inserire una parola nel prompt 
-const parolaUtente = prompt("Scrivi una parola")
+const parolaUtente = prompt("Scrivi una parola palindroma")
    
 //creare una funzione che compara la parola palindroma con la parola inserita dall'utente
 
 if (parolaUtente){
     if (parolaPalindroma(parolaUtente)) {
-        console.log("La parola corrisponde a 'palindroma'")
+        console.log("La parola e un 'palindroma'")
     } else {
-        console.log("La parola NON CORRISPONDE a 'palindroma'")
+        console.log("La parola NON E un 'palindroma', scrivi una parola palindroma")
     }
-}else {
-    console.log("scrivi una parola")
 }
-
 /* function parolaPalindroma() */
 
 function parolaPalindroma(parola){
-    return parola === "palindroma"
+    return parola === parola.split("").reverse().join("");
 }
 
 
