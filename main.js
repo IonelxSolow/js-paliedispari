50cfc13a33495ae3ed6cdbb3a74099a39222eb30
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-14 16:42:49 +0100

    add the function that compares the word entered by the user with the palindrome word

--- main.js
+++ main.js
@@ -2,9 +2,29 @@
 //PALINDROMA
 
 //indicare all'utente di inserire una parola nel prompt 
-
-
+const parolaUtente = prompt("Scrivi una parola")
+   
 //creare una funzione che compara la parola palindroma con la parola inserita dall'utente
+
+if (parolaUtente){
+    if (parolaPalindroma(parolaUtente)) {
+        console.log("La parola corrisponde a 'palindroma'")
+    } else {
+        console.log("La parola NON CORRISPONDE a 'palindroma'")
+    }
+}else {
+    console.log("scrivi un'altra parola")
+}
+
+/* function parolaPalindroma() */
+
+function parolaPalindroma(parola){
+    return parola === "palindroma"
+}
+
+
+
+
 
 
 //PARI E DISPARI
